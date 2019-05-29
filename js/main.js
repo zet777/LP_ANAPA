7b4d0fa3587a63f48a1ef8b1dacd56b86e7112db
Roman Tashev <rzet777@gmail.com>
2019-05-29 16:57:34 +0300

    Add burger menu

--- js/main.js
+++ js/main.js
@@ -330,3 +330,11 @@ jQuery(document).ready(function($) {
 	windowScrolled();
 
 });
+$('.card').on('click', function () {
+	$('.container').toggleClass('container-origin');
+})
+$('.menu-btn').on('click', function (e) {
+	e.preventDefault; /*отменяем стандартное событие*/
+	$(this).toggleClass('menu-btn_active');
+	$('.menu-nav').toggleClass('menu-nav_active');
+})
