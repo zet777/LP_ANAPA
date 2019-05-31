3234613b2939bb20deb339a482442abdd0eb4cb5
Roman Tashev <rzet777@gmail.com>
2019-05-31 14:02:02 +0300

    Update transform.js

--- js/transform.js
+++ js/transform.js
@@ -3,3 +3,8 @@ $('.menu-btn').on('click', function (e) {
 	$(this).toggleClass('menu-btn_active');
 	$('.menu-nav').toggleClass('menu-nav_active');
 })
+$('.menu-btn').on('click', function (e) {
+	e.preventDefault; /*отменяем стандартное событие*/
+	$('menu').toggleClass('d-none');
+	$('.menu').toggleClass('d-none');
+})
